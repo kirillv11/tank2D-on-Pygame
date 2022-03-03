b55a4cf53285026fd53d90fadb95ac652777207c
Elkin kirill <74110907+kirillv11@users.noreply.github.com>
2022-03-03 17:34:05 +0300

    Update map.py

--- map.py
+++ map.py
@@ -1,48 +1,38 @@
 import pygame
-import random
+from random import randint
 
 img_rect = []
 img = []
-img_rect_2 = []
-img_2 = []
-map_spicok = ["2", "3", "1", "2", "3", "1", "2", "3", "1"]  # здесь все карты
-map_random = map_spicok[random.randint(0, 8)]  # выбираем карту
+img_rect_grass = []
+img_grass = []
+map_spicok = ["1", "2", "3", "1", "2", "3", "1", "2", "3"]  # здесь все карты
+map_random = map_spicok[randint(0, 8)]
 def create_map():
     with open("map" + map_random + ".txt", "r+", encoding="utf-8") as file:
-        h = 0
-        w = 0
+        w, h = 0, 0
         for line in file:
             for i in range(len(line)):
-                if line[i] != "\n":
+                if line[i] != "\n" and len(img_rect) < 200:
                     if line[i] == "#":
                         wall_rect = pygame.Rect(w, h, 50, 50)
-                        if len(img_rect) < 200:
-                            img_rect.append(wall_rect)
-                            img.append("wall")
+                        img_rect.append(wall_rect)
+                        img.append("wall")
                     elif line[i] == "/":
                         grass_rect = pygame.Rect(w, h, 50, 50)
-                        if len(img_rect) < 200:
-                            pass
-                            img_rect_2.append(grass_rect)
-                            img_2.append("grass")
+                        img_rect_grass.append(grass_rect)
+                        img_grass.append("grass")
                     elif line[i] == "+":
                         box_rect = pygame.Rect(w, h, 50, 50)
-                        if len(img_rect) < 200:
-                            img_rect.append(box_rect)
-                            img.append("box")
+                        img_rect.append(box_rect)
+                        img.append("box")
                     elif line[i] == "~":
                         water_rect = pygame.Rect(w, h, 50, 50)
-                        if len(img_rect) < 200:
-                            img_rect.append(water_rect)
-                            img.append("water")
+                        img_rect.append(water_rect)
+                        img.append("water")
 
                 w += 50
 
             w = 0
             h += 50
-
+        file.close()
         return img_rect, img, map_random
-
-
-
-
