578517d760adefb7c6132ad8b37e035626722f47
Elkin Kirill <74110907+kirillv11@users.noreply.github.com>
2022-07-05 16:54:26 +0300

    Update map.py

--- map.py
+++ map.py
@@ -1,38 +1,35 @@
 import pygame
 from random import randint
 
-img_rect = []
-img = []
-img_rect_grass = []
-img_grass = []
-map_spicok = ["1", "2", "3", "1", "2", "3", "1", "2", "3"]  # здесь все карты
-map_random = map_spicok[randint(0, 8)]
+img_rect, img, img_rect_grass, img_grass = [], [], [], []
+num_of_map = str(randint(1, 3))
+
+
 def create_map():
-    with open("map" + map_random + ".txt", "r+", encoding="utf-8") as file:
+    with open("map" + num_of_map + ".txt", "r+", encoding="utf-8") as file:
         w, h = 0, 0
         for line in file:
-            for i in range(len(line)):
-                if line[i] != "\n" and len(img_rect) < 200:
-                    if line[i] == "#":
-                        wall_rect = pygame.Rect(w, h, 50, 50)
-                        img_rect.append(wall_rect)
-                        img.append("wall")
-                    elif line[i] == "/":
-                        grass_rect = pygame.Rect(w, h, 50, 50)
-                        img_rect_grass.append(grass_rect)
-                        img_grass.append("grass")
-                    elif line[i] == "+":
-                        box_rect = pygame.Rect(w, h, 50, 50)
-                        img_rect.append(box_rect)
-                        img.append("box")
-                    elif line[i] == "~":
-                        water_rect = pygame.Rect(w, h, 50, 50)
-                        img_rect.append(water_rect)
-                        img.append("water")
-
+            for i in line:
+                if i != "\n" and len(img_rect) < 200:
+                    match i:
+                        case '#':
+                            wall_rect = pygame.Rect(w, h, 50, 50)
+                            img_rect.append(wall_rect)
+                            img.append("wall")
+                        case '/':
+                            grass_rect = pygame.Rect(w, h, 50, 50)
+                            img_rect_grass.append(grass_rect)
+                            img_grass.append("grass")
+                        case '+':
+                            box_rect = pygame.Rect(w, h, 50, 50)
+                            img_rect.append(box_rect)
+                            img.append("box")
+                        case '~':
+                            water_rect = pygame.Rect(w, h, 50, 50)
+                            img_rect.append(water_rect)
+                            img.append("water")
                 w += 50
-
             w = 0
             h += 50
         file.close()
-        return img_rect, img, map_random
+        return img_rect, img, num_of_map
